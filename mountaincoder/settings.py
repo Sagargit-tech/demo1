3d4ea9579caf634e1260c2f5c9fd102b590de1a9
Sagargit-tech <78584787+Sagargit-tech@users.noreply.github.com>
2021-06-07 19:29:28 +0530

    Update settings.py

--- mountaincoder/settings.py
+++ mountaincoder/settings.py
@@ -114,13 +114,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[BASE_DIR,'static']
+STATICFILES_DIRS=[BASE_DIR,'static',os.path.join(BASE_DIR, "static/")]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
